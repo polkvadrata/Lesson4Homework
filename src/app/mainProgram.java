088353e76c84d32627e141487e901651f0ac3031
polkvadrata <polkvadrata@gmail.com>
2024-09-25 15:13:52 +0300

    change solution

--- src/app/mainProgram.java
+++ src/app/mainProgram.java
@@ -4,33 +4,33 @@ public class mainProgram {
 
     static int productNumber;
     static int days;
-    static double productSum;
-    static double salesByDay;
+    static double totalProductSum;
+    static double productPrice;
     final static String currency = "EUR";
 
     public static void main(String[] arg) {
 
         productNumber = 1;
         days = 5;
-        productSum = 12153.41;
-        salesByDay = productSum / days;
+        productPrice = 2430.682;
+        totalProductSum = productPrice * days;
 
         System.out.printf(
                 "%nProduct No %x: smartphone," +
-                        "%ntotal sales for %x day is %s %.2f," +
-                        "%nsales by day is %s %.2f."
-                ,productNumber,days,currency,productSum,currency,salesByDay);
+                "%ntotal sales for %x day is %s %.2f," +
+                "%nsales by day is %s %.2f."
+                ,productNumber,days,currency,totalProductSum,currency,productPrice);
 
         productNumber = 2;
         days = 7;
-        productSum = 10486.85;
-        salesByDay = productSum / days;
+        productPrice = 1498.121;
+        totalProductSum = productPrice * days;
 
         System.out.printf(
                 "%nProduct No %x: smartphone," +
-                        "%ntotal sales for %x day is %s %.2f," +
-                        "%nsales by day is %s %.2f."
-                ,productNumber,days,currency,productSum,currency,salesByDay);
+                "%ntotal sales for %x day is %s %.2f," +
+                "%nsales by day is %s %.2f."
+                ,productNumber,days,currency,totalProductSum,currency,productPrice);
     }
 
 }
